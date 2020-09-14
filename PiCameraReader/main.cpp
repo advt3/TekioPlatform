4be14e34dfb58161e3cd0fb779f10aa7cf3f1c61
Santiago Hurtado <s-hurtad@uniandes.edu.co>
2020-09-14 10:14:21 +0200

    Enable cross compilation for ARM

--- PiCameraReader/main.cpp
+++ PiCameraReader/main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
-#include <opencv2/core.hpp>
+/*#include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/highgui.hpp>*/
 
 int main() {
-  cv::Mat frame,flipped;
-  cv::VideoCapture cap;
-  cap.open(0);
-  cap.read(frame);
-  if(!frame.empty()){
-    cv::flip(frame,flipped, -1);
-    cv::imwrite("/tmp/frame2.jpg",flipped);
-  }else{
-    std::cout << "No frame read" << std::endl;
-  }
+  std::cout << "Hello World"<<std::endl;
   return 0;
 }
